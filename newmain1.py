b1da7e9b6334acf63d4fac1d0407b21d1fb84a3a
Alex Zhang <zjxfun@outlook.com>
2025-05-20 14:58:01 +0800

    优化

--- newmain1.py
+++ newmain1.py
@@ -3,7 +3,7 @@
 import utils
 import logging
 import work_flow
-import work_flow_new
+import work_flow_new1
 import settings
 import schedule
 import time
@@ -13,7 +13,7 @@ from pathlib import Path
 def job():
     if utils.is_weekday():
         work_flow.prepare()
-        work_flow_new.prepare()
+        work_flow_new1.prepare()
 
 
 logging.basicConfig(format='%(asctime)s %(message)s', filename='sequoia.log')
@@ -28,5 +28,5 @@ if settings.config['cron']:
         schedule.run_pending()
         time.sleep(1)
 else:
-    work_flow_new.prepare()
+    work_flow_new1.prepare()
     # work_flow.prepare()
